1aaab32e17b90bf2893973d9f28be9a57264c194
Luan Rocha <kuro.winchester@gmail.com>
2023-09-25 19:06:21 -0300

    .

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,11 +1,13 @@
-export default function ehUmCPF(campo) {
-    const cpf = campo.value.replace(/\.|-/g, "");
-    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        campo.setCustomValidity('Esse cpf não é válido')
+export default function ehUmCpf(campo){
+    const cpf = campo.value.replace(/\.|-/g, '');
+    if (validarNumerosRepetidos(cpf) || validarPrimeiroDigito(cpf) || validarSegundoDigito(cpf)){
+        console.log('CPF inválido');
+    } else{
+        console.log('CPF válido');
     }
 }
 
-function validaNumerosRepetidos(cpf) {
+function validarNumerosRepetidos(cpf){
     const numerosRepetidos = [
         '00000000000',
         '11111111111',
@@ -18,42 +20,39 @@ function validaNumerosRepetidos(cpf) {
         '88888888888',
         '99999999999'
     ]
-
-    return numerosRepetidos.includes(cpf)
+    return numerosRepetidos.includes(cpf);
 }
 
-function validaPrimeiroDigito(cpf) {
-    let soma = 0;
-    let multiplicador = 10;
-
-    for (let tamanho = 0; tamanho < 9; tamanho++) {
-        soma += cpf[tamanho] * multiplicador;
+function validarPrimeiroDigito(cpf){
+    let soma = 0
+    let multiplicador = 10
+    for(let i = 0; i < 9; i++){
+        soma += cpf[i] * multiplicador
         multiplicador--
     }
 
-    soma = (soma * 10) % 11;
+    soma = (soma * 10) % 11
 
-    if (soma == 10 || soma == 11) {
-        soma = 0;
+    if(soma == 10 || soma == 11){
+        soma = 0
     }
-
-    return soma != cpf[9];
+    
+    return soma != cpf[9]
 }
 
-function validaSegundoDigito(cpf) {
-    let soma = 0;
-    let multiplicador = 11;
-
-    for (let tamanho = 0; tamanho < 10; tamanho++) {
-        soma += cpf[tamanho] * multiplicador;
+function validarSegundoDigito(cpf){
+    let soma = 0
+    let multiplicador = 11
+    for(let i = 0; i < 10; i++){
+        soma += cpf[i] * multiplicador
         multiplicador--
     }
+    
+    soma = (soma * 10) % 11
 
-    soma = (soma * 10) % 11;
-
-    if (soma == 10 || soma == 11) {
-        soma = 0;
+    if(soma == 10 || soma == 11){
+        soma = 0
     }
-
-    return soma != cpf[10];
+    
+    return soma != cpf[10]
 }
