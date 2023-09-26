a232f8da5ff124f25b8d4f72216063c9ad459053
Luan Rocha <kuro.winchester@gmail.com>
2023-09-25 21:35:57 -0300

    validação de idade e cpf

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-import ehUmCpf from './valida-cpf.js';
-const camposdoFormulario = document.querySelectorAll('required');
+import ehUmCpf from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js"; 
+const camposdoFormulario = document.querySelectorAll('[required]');
 
 camposdoFormulario.forEach((campo) => {
-    campo.addEventListener('blur', () => verificaCampo(campo));
+    campo.addEventListener("blur", () => verificaCampo(campo));
 });
 
 function verificaCampo(campo) {
     if(campo.name == "cpf" && campo.value.length >= 11){
         ehUmCpf(campo);
+    }
+    if(campo.name == "aniversario" && campo.value != ''){
+        ehMaiorDeIdade(campo);
     }
 }
